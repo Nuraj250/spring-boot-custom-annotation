ee2b5dc6cca3b7bfd94a41bb66cad3f8373399ab
Nuraj250 <nurajshaminda200@gmail.com>
2025-01-04 22:21:19 -0600

    done

--- src/main/java/com/example/customAnnotation/aspets/LogExecutionTimeAspect.java
+++ src/main/java/com/example/customAnnotation/aspets/LogExecutionTimeAspect.java
@@ -2,25 +2,24 @@ package com.example.customAnnotation.aspets;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-
-import java.util.logging.Logger;
 
 @Aspect
 @Component
 public class LogExecutionTimeAspect {
 
-    private static final Logger logger = (Logger) LoggerFactory.getLogger(LogExecutionTimeAspect.class);
+    private static final Logger logger = LoggerFactory.getLogger(LogExecutionTimeAspect.class);
 
-    @Around("@annotation(com.example.annotations.LogExecutionTime)")
+    @Around("@annotation(com.example.customAnnotation.annotations.LogExecutionTime)")
     public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
         long startTime = System.currentTimeMillis();
 
-        Object proceed = joinPoint.proceed(); // Execute the method
+        Object proceed = joinPoint.proceed();
 
         long executionTime = System.currentTimeMillis() - startTime;
-        logger.info("{} executed in {} ms");
+        logger.info("{} executed in {} ms", joinPoint.getSignature(), executionTime);
 
         return proceed;
     }
